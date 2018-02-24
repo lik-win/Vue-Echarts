0d4e4231e41dc0f8c6c8e88f2647cfa71a29a264
likai <likai@dituwuyou.com>
2018-02-24 14:58:24 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <div id="myECharts"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+    }
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById('myECharts'))
+      var data = [];
+
+      for (var i = 0; i <= 100; i++) {
+        var theta = i / 100 * 360;
+        var r = 5 * (1 + Math.sin(theta / 180 * Math.PI));
+        data.push([r, theta]);
+      }
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: '极坐标双数值轴'
+        },
+        legend: {
+          data: ['line']
+        },
+        polar: {},
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        angleAxis: {
+          type: 'value',
+          startAngle: 0
+        },
+        radiusAxis: {
+        },
+        series: [{
+          coordinateSystem: 'polar',
+          name: 'line',
+          type: 'line',
+          showSymbol: false,
+          data: data
+        }]
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+#myECharts {
+  width: 900px;
+  height: 600px;
+}
+</style>
